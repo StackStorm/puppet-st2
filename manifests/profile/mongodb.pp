80a9b25820c688a75a12140ee31a998b8d06283e
James Fryman <james@fryman.io>
2015-01-13 16:21:20 -0600

    revert back to original

--- manifests/profile/mongodb.pp
+++ manifests/profile/mongodb.pp
@@ -5,15 +5,6 @@ class st2::profile::mongodb {
     class { '::mongodb::server': }
   }
 
-  # Install from upstream servers if RHEL
-  if $::osfamily == 'RedHat' {
-    if !defined(Class['::mongodb::globals']) {
-      class { '::mongodb::globals':
-        manage_package_repo => true,
-      }
-    }
-  }
-
   $_mongodb_dependencies = $::osfamily ? {
     'Debian' => $::st2::params::debian_mongodb_dependencies,
     default  => undef,
