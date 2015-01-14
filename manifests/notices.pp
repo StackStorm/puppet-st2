16fb3c6eeadbf389bc6c92db23865ef81531fb7c
James Fryman <james@fryman.io>
2015-01-14 14:16:49 -0600

    Update notices.pp

--- manifests/notices.pp
+++ manifests/notices.pp
@@ -17,6 +17,6 @@ class st2::notices {
   $user_missing_client_keys = "ssh_public_key and ssh_key_type need to be supplied for this resource. Help can be found in INSTALL.md if needed"
   $user_missing_private_key = "ssh_private_key needs to be supplied for this resource. Help can be found in INSTALL.md if needed"
   $unsupported_os = "Your platform is not yet supported. Please file a bug or submit a bug to $st2::params::repo_url"
-  $web_no_oauth_token = "The Web Interface is currently in limited beta. You will need a key to test this out. Please email us at XXX@stackstorm.com if you are interested in trying it out and providing feedback"
+  $web_no_oauth_token = "The Web Interface is currently in limited beta. You will need a key to test this out. Please email us at support@stackstorm.com if you are interested in trying it out and providing feedback"
 
 }
