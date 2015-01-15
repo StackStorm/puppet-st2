d3915baa14134a9fabb36bf3fc74011a9053be32
James Fryman <james@fryman.io>
2015-01-15 13:20:19 -0600

    Update nodejs.pp

--- manifests/profile/nodejs.pp
+++ manifests/profile/nodejs.pp
@@ -22,12 +22,6 @@ class st2::profile::nodejs {
     notify      => Exec['upgrade npm'],
   }
 
-  exec { 'upgrade npm':
-    command     => 'npm install npm -g --ca=""',
-    path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-    refreshonly => true,
-  }
-
   package { 'bower':
     ensure   => present,
     provider => 'npm',
@@ -38,6 +32,4 @@ class st2::profile::nodejs {
     provider => 'npm',
     require  => Class['::nodejs'],
   }
-
-  Exec['upgrade npm'] -> Package<| provider == 'npm' |>
 }
