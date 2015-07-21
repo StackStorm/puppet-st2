3e11fababd21077ff95e5cc62867de73483fd3d6
James Fryman <james@fryman.io>
2015-07-21 17:45:09 -0500

    make unstable actually that

--- lib/puppet/parser/functions/st2_latest_unstable.rb
+++ lib/puppet/parser/functions/st2_latest_unstable.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 
 module Puppet::Parser::Functions
   newfunction(:st2_latest_stable, :type => :rvalue) do |args|
-     page = open("https://downloads.stackstorm.net/deb/pool/trusty_stable/main/s/st2api/").read
+     page = open("https://downloads.stackstorm.net/deb/pool/trusty_unstable/main/s/st2api/").read
      page.split.select { |x| x =~ /href=\"st2api/ }.collect { |x| x.scan(/>(.*)</) }.flatten.collect { |x| x.scan(/_(.*)-/).first[0] }.sort_by { |x| x.split('.')[2].to_i * -1 }.sort_by { |x| x.split('.')[1].to_i * -1}.sort_by { |x| x.split('.')[0].to_i * -1}.first
   end
 end
