108ff908d0dedb1da48837bef91d4ced0e836d7f
Patrick Hoolboom <patrick@stackstorm.com>
2015-09-14 14:55:53 -0700

    REmoved trailing white space

--- manifests/profile/fullinstall.pp
+++ manifests/profile/fullinstall.pp
@@ -14,7 +14,7 @@
 #  include st2::profile::fullinstall
 #
 class st2::profile::fullinstall inherits st2 {
-  class { '::st2::profile::repos': 
+  class { '::st2::profile::repos':
     before => Anchor['st2::bootstrap']
   }
 
