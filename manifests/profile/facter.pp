384453bc481e76fffe5fb97ff94b2eef267f3e44
Patrick Hoolboom <patrick@stackstorm.com>
2015-09-16 14:18:20 -0700

    Ensure facter directory exists on all hosts

--- manifests/profile/facter.pp
+++ manifests/profile/facter.pp
@@ -16,20 +16,18 @@
 #
 class st2::profile::facter {
   # The hackery is strong.  Not sure where else to hack this in.
-  if $::osfamily == "RedHat" {
-    file{'/etc/facter':
-      ensure  => 'directory',
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0444'
-    }
+  file{'/etc/facter':
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444'
+  }
 
-    file{'/etc/facter/facts.d':
-      ensure  => 'directory',
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0444',
-      require => File['/etc/facter']
-    }
+  file{'/etc/facter/facts.d':
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444',
+    require => File['/etc/facter']
   }
 }
