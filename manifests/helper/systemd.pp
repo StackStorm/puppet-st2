42e69877b113d1cdc3f52d87d92578e9ea61d6b4
Patrick Hoolboom <patrick@stackstorm.com>
2015-10-07 15:31:42 -0700

    Only enable singles

--- manifests/helper/systemd.pp
+++ manifests/helper/systemd.pp
@@ -21,10 +21,12 @@ define st2::helper::systemd (
     content => template("st2/etc/systemd/system/st2service_${process_type}.service.erb"),
   }
 
-  exec{'sysctl enable':
-    path    => '/bin:/usr/bin:/usr/local/bin',
-    command => "systemctl --no-reload enable ${st2_process}",
-    require => File["/etc/systemd/system/${st2_process}${type}.service"],
-    notify  => Service["${st2_process}"],
+  if $process_type == 'single' {
+    exec{'sysctl enable':
+      path    => '/bin:/usr/bin:/usr/local/bin',
+      command => "systemctl --no-reload enable ${st2_process}",
+      require => File["/etc/systemd/system/${st2_process}${type}.service"],
+      notify  => Service["${st2_process}"],
+    }
   }
 }
