df9c4e6a50270d4ace296ffe5b26e9aacba2f7c6
Patrick Hoolboom <patrick@stackstorm.com>
2015-10-08 10:27:27 -0700

    Update service_manager.pp

--- manifests/helper/service_manager.pp
+++ manifests/helper/service_manager.pp
@@ -3,24 +3,24 @@
 #  This deined type is used to add service management scripts for the various distros
 #
 define st2::helper::service_manager (
-  $st2_process = undef,
+  $process = undef,
 ) {
 
   $init_provider = $::st2::params::init_type
 
   if $osfamily == 'Debian' {
 
-    file { "/etc/init/${st2_process}.conf":
+    file { "/etc/init/${process}.conf":
       ensure => present,
       owner  => 'root',
       group  => 'root',
       mode   => '0444',
-      source => "puppet:///modules/st2/etc/init/${st2_process}.conf",
-      notify => Service["${st2_process}"],
+      source => "puppet:///modules/st2/etc/init/${process}.conf",
+      notify => Service["${process}"],
     }
   } elsif $osfamily == 'RedHat' {
     if $operatingsystemmajrelease == '7' {
-      if $st2_process == 'st2actionrunner' {
+      if $process == 'st2actionrunner' {
         $process_type = 'multi'
         file{"/etc/systemd/system/st2actionrunner.service":
           ensure  => file,
@@ -33,18 +33,18 @@ define st2::helper::service_manager (
         exec{'sysctl enable':
           path    => '/bin:/usr/bin:/usr/local/bin',
           command => "systemctl --no-reload enable st2actionrunner",
-          require => File["/etc/systemd/system/${st2_process}${type}.service"],
-          notify  => Service["${st2_process}"],
+          require => File["/etc/systemd/system/${process}${type}.service"],
+          notify  => Service["${process}"],
         }
 
-        st2::helper::systemd{ "${st2_process}s_systemd":
-          st2_process  => $st2_process,
+        st2::helper::systemd{ "${process}_multi_systemd":
+          st2_process  => $process,
           process_type => $process_type
         }
       } else {
         $process_type = 'single'
-        st2::helper::systemd{ "${st2_process}_systemd":
-          st2_process  => $st2_process,
+        st2::helper::systemd{ "${process}_systemd":
+          st2_process  => $process,
           process_type => $process_type
         }
       }
@@ -53,7 +53,7 @@ define st2::helper::service_manager (
     }
   }
 
-  service { "${st2_process}":
+  service { "${process}":
     ensure     => running,
     enable     => true,
     hasstatus  => true,
