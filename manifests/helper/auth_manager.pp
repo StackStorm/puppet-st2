bc4b0517683543e64cc021fd90ceaf5f872e6e70
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2015-10-14 10:59:52 -0700

    Auth adapter

---
+++ manifests/helper/auth_manager.pp
@@ -0,0 +1,143 @@
+# Definition: st2::helper::auth_manager
+#
+#  This defined type is used to configure various kinds of auth plugins for st2
+#
+define st2::helper::auth_manager (
+  $auth_mode = $auth_mode,  # proxy or standalone
+  $auth_backend = $auth_backend,
+  $debug = $debug,
+  $test_user = $test_user,
+) {
+  $_debug = $debug ? {
+    true    => 'True',
+    default => 'False',
+  }
+  $_api_url = $::st2::params::api_url
+  $_st2_conf_file = $::st2::params::conf_file
+  $_st2_api_logging_file  = $::st2::params::api::logging_file
+  $_use_ssl = $::st2::params::use_ssl_for_auth_api
+  $_ssl_key = $::st2::params::api::ssl_key
+  $_ssl_cert = $::st2::params::api::ssl_cert
+  $_auth_users = hiera_hash('st2::params::auth_users', {})
+  $_cli_username = $::st2::params::cli_username
+  $_cli_password = $::st2::params::cli_password
+
+  tag('st2::auth_manager')
+
+  # Common settings for all auth backends
+
+  ini_setting { 'auth_debug':
+    ensure  => present,
+    path    => "${_st2_conf_file}",
+    section => 'auth',
+    setting => 'debug',
+    value   => $_debug,
+  }
+  ini_setting { 'auth_api_url':
+    ensure  => present,
+    path    => "${_st2_conf_file}",
+    section => 'auth',
+    setting => 'api_url',
+    value   => $_api_url,
+  }
+  ini_setting { 'auth_mode':
+    ensure  => present,
+    path    => "${_st2_conf_file}",
+    section => 'auth',
+    setting => 'mode',
+    value   => $auth_mode,
+  }
+
+  if $auth_mode == 'standalone' {
+    ini_setting { 'auth_backend':
+      ensure  => present,
+      path    => "${_st2_conf_file}",
+      section => 'auth',
+      setting => 'backend',
+      value   => "${_auth_backend}",
+    }
+    ini_setting { 'auth_logging_file':
+      ensure  => present,
+      path    => "${_st2_conf_file}",
+      section => 'auth',
+      setting => 'logging',
+      value   => "${_st2_api_logging_file}",
+    }
+    ini_setting { 'auth_ssl':
+      ensure  => present,
+      path    => "${_st2_conf_file}",
+      section => 'auth',
+      setting => 'use_ssl',
+      value   => "${_use_ssl}",
+    }
+
+    # System Users
+    $_testuser_ensure = $test_user ? {
+      true    => present,
+      default => absent,
+    }
+    st2::auth_user { 'testu':
+      ensure    => $_testuser_ensure,
+      password => 'testp',
+    }
+    st2::auth_user { $_cli_username:
+      password => $_cli_password,
+    }
+
+    if $test_user {
+      notify { $::st2::notices::auth_test_user_enabled: }
+    }
+
+    # Automatically generate users from Hiera
+    create_resources('st2::auth_user', $_auth_users)
+
+    # SSL Settings
+    if $_use_ssl {
+      if !$ssl_cert or !$ssl_key {
+        fail('[st2::auth::standalone] Missing $ssl_cert or $ssl_key to enable SSL')
+      }
+
+      ini_setting { 'auth_ssl_cert':
+        ensure  => present,
+        path    => "${_st2_conf_file}",
+        section => 'auth',
+        setting => 'cert',
+        value   => "${_ssl_cert}",
+      }
+      ini_setting { 'auth_ssl_key':
+        ensure  => present,
+        path    => "${_st2_conf_file}",
+        section => 'auth',
+        setting => 'key',
+        value   => "${_ssl_key}",
+      }
+    }
+
+    # Backend specific ini setttings
+
+    $_auth_backend_kwargs = ''
+
+    case $_auth_backend {
+      'proxy': {
+        # Nothing to set for now
+      }
+      'pam': {
+        # Nothing to set for now
+      }
+      'mongodb': {
+        $_db_host = $::st2::params::db::host
+        $_db_port = $::st2::params::db::port
+        $_db_name = $::st2::params::db::name
+        $_auth_backend_kwargs = "{\"db_host\": \"${_db_host}\", \"db_port\": \"${_db_port}\", \"db_name\": \"${_db_name}\"}"
+      }
+    }
+
+    ini_setting { 'auth_backend_kwargs':
+      ensure  => present,
+      path    => '/etc/st2/st2.conf',
+      section => 'auth',
+      setting => 'backend_kwargs',
+      value   => "${_auth_backend_kwargs}",
+    }
+  }
+}
