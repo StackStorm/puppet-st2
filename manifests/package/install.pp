b35a887a2da8a079054bbd8ed89cf07e2484e1a1
James Fryman <james@fryman.io>
2015-10-19 10:52:54 -0500

    use correct variable for comparision

--- manifests/package/install.pp
+++ manifests/package/install.pp
@@ -53,7 +53,7 @@ define st2::package::install(
         # A very odd RHEL 6 bug we have not been able to get to the bottom of yet
         # 0.14dev.184-1 becomes 0.14.dev184-1
         # https://stackstorm.slack.com/archives/opstown/p1445224799003958
-        if $::operatingsystemmajrelease == '6' and $name =~ /dev/ {
+        if $::operatingsystemmajrelease == '6' and $version =~ /dev/ {
           $_package_version = regsubst("${_version}.${_revision}-1", 'dev\.', '\.dev')
         } else {
           $_package_version = "${_version}.${_revision}-1"
