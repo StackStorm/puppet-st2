ea7bcdbb02f0475a4f94584bd625be19253ed5f5
Tomaz Muraus <tomaz@tomaz.me>
2016-01-08 15:58:38 +0100

    Update docstring.

--- manifests/stanley.pp
+++ manifests/stanley.pp
@@ -11,14 +11,6 @@
 #  [*client*]          - Allow incoming connections from the defined user (default: true)
 #  [*server*]          - Server where connection requests originate (usually st2 server) (default: false)
 #
-# === Variables
-#  [*default_ssh_public_key*]  - Default SSH Public Key without leading key-type and end email
-#  [*default_ssh_key_type*]    - Default SSH Key (ssh-dsa/ssh-rsa)
-#  [*default_ssh_private_key*] - Default Private Key
-#  [*_ssh_public_key*]         - Local variable holding the real value of `ssh_public_key` (set or default)
-#  [*_ssh_key_type*]           - Local variable holding the real value of `ssh_key_type` (set or default)
-#  [*_ssh_private_key*]        - Local variable holding the real value of `ssh_private_key` (set or default)
-#
 # === Examples
 #
 #  include ::st2::stanley
