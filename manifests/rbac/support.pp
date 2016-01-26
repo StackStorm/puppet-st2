c3dc75a1cdbf0d5e93e04f5000c3f87104aaf438
Tomaz Muraus <tomaz@tomaz.me>
2016-01-26 18:52:26 +0100

    Fix syntax error.

--- manifests/rbac/support.pp
+++ manifests/rbac/support.pp
@@ -2,41 +2,41 @@ class st2::rbac::support {
   $_rbac_dir = '/opt/stackstorm/rbac'
 
   file { $_rbac_dir:
-    'ensure'  => 'directory',
-    'owner'   => 'root',
-    'group'   => 'root',
-    'mode'    => '0755',
-    'recurse' => true,
-    'require' => Class['::st2::profile::server'],
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    recurse => true,
+    require => Class['::st2::profile::server'],
   }
 
   file "${_rbac_dir}/assignments":
-    'ensure'  => 'directory',
-    'owner'   => 'root',
-    'group'   => 'root',
-    'mode'    => '0755',
-    'require' => Class['::st2::profile::server'],
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Class['::st2::profile::server'],
   }
 
   file { "${_rbac_dir}/roles":
-    'ensure'  => 'directory',
-    'owner'   => 'root',
-    'group'   => 'root',
-    'mode'    => '0755',
-    'require' => Class['::st2::profile::server'],
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Class['::st2::profile::server'],
   }
 
   file { "${_rbac_dir}/assignments":
-    'ensure'  => 'directory',
-    'owner'   => 'root',
-    'group'   => 'root',
-    'mode'    => '0755',
-    'require' => Class['::st2::profile::server'],
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Class['::st2::profile::server'],
   }
 
   exec { 'reload st2 rbac definitions':
-    'command'         => 'st2-apply-rbac-definitions',
-    'refreshonly'     => 'true',
-    'path'            => '/usr/sbin:/usr/bin:/sbin:/bin',
+    command         => 'st2-apply-rbac-definitions',
+    refreshonly     => 'true',
+    path            => '/usr/sbin:/usr/bin:/sbin:/bin',
   }
 }
