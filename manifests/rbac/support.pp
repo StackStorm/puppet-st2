e03b22ce3c649ec923a0b4fcc6a298c8b1294d98
Tomaz Muraus <tomaz@tomaz.me>
2016-01-26 19:33:35 +0100

    Remove duplicate definition.

--- manifests/rbac/support.pp
+++ manifests/rbac/support.pp
@@ -26,14 +26,6 @@ class st2::rbac::support {
     require => Class['::st2::profile::server'],
   }
 
-  file { "${_rbac_dir}/assignments":
-    ensure  => 'directory',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
-    require => Class['::st2::profile::server'],
-  }
-
   exec { 'reload st2 rbac definitions':
     command         => 'st2-apply-rbac-definitions',
     refreshonly     => 'true',
