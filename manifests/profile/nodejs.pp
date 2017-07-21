d841a6474f9ebdd692775fa5eda1445e3d5f778b
Nick Maludy <nmaludy@gmail.com>
2017-07-20 22:18:13 -0400

    Got NodeJS install working

--- manifests/profile/nodejs.pp
+++ manifests/profile/nodejs.pp
@@ -1,5 +1,18 @@
 # Class: st2::profile::nodejs
 #
 class st2::profile::nodejs {
-  require ::nodejs
+  # Red Hat 7.x + already have NodeJS 6.x+ installed
+  # trying to install from nodesource repos fails, so just use the builtin
+  if $::osfamily == 'RedHat' and $::operatingsystemmajrelease >= '7' {
+    class { '::nodejs':
+      manage_package_repo => false,
+      npm_package_ensure  => 'present',
+    }
+  }
+  else {
+    # else install nodejs 4.x from nodesource repo
+    class { '::nodejs':
+      repo_url_suffix    => '4.x',
+    }
+  }
 }
