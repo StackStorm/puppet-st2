8c7be4971058f37ecddc47a26329a572224375dd
Nick Maludy <nmaludy@gmail.com>
2017-08-03 13:38:36 -0400

    Added facts to auth spec

--- spec/defines/auth_user_spec.rb
+++ spec/defines/auth_user_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
+require_relative '../helpers/fact_helper'
 
 describe 'st2::auth_user' do
   let(:title) { 'stanley' }
   describe 'creates an htpasswd entry' do
-    let(:params) {
+    let (:facts) { FactHelper.ubuntu_trusty_x64 }
+    let (:params) {
       {
         :ensure   => 'present',
         :password => 'sekritp3sswrd',
