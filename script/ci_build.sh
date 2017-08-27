074ec4c2045ea2ce3c5ad58ed840245d66d80b36
Nick Maludy <nmaludy@gmail.com>
2017-08-27 10:37:20 -0400

    First attempt at kitchen ci

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -3,4 +3,5 @@
 facter && \
   bundle exec rake validate && \
   bundle exec rake lint && \
-  bundle exec rake spec SPEC_OPTS='--format documentation'
+  bundle exec rake spec SPEC_OPTS='--format documentation' && \
+  if [ -z ${DISTRO} ]; then kitchen test ${DISTRO}; else echo "Not running kitchenci because DISTRO not set"; fi
