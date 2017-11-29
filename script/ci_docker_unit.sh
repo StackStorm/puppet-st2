115ec64f23c0ec73ac90d9cb559244a4f80016f7
Nick Maludy <nmaludy@gmail.com>
2017-11-28 23:46:50 -0500

    Moved all commands into a script

--- script/ci_docker_unit.sh
+++ script/ci_docker_unit.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+set -e
+set -o xtrace
 
-docker build -t stackstorm/puppet-st2-$DISTRO -f $BUILD_PATH/Dockerfile .
+docker build -t stackstorm/puppet-st2-$DISTRO -f $BUILD_PATH/Dockerfile
 docker run -dit --name stackstorm-puppet-st2-$DISTRO stackstorm/puppet-st2-$DISTRO
 docker exec stackstorm-puppet-st2-$DISTRO bash -c "bundle exec rake validate"
 docker exec stackstorm-puppet-st2-$DISTRO bash -c "bundle exec rake lint"
--- script/ci_docker_unit_clean.sh
+++ script/ci_docker_unit_clean.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+set -o xtrace
+
 docker rm -f stackstorm-puppet-st2-$DISTRO &> /dev/null
 docker images -q stackstorm/puppet-st2-$DISTRO | xargs --no-run-if-empty docker image rm
