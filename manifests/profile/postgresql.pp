57110adf0febfb2f3fe2814067559c35eeaf3308
Nick Maludy <nmaludy@gmail.com>
2017-12-11 16:05:43 -0500

    Reverting back CentOS6 changes after upstream postrgres bugfix

--- manifests/profile/postgresql.pp
+++ manifests/profile/postgresql.pp
@@ -21,23 +21,11 @@ class st2::profile::postgresql(
   $db_password = $st2::db_password,
   $db_listen_addresses = '127.0.0.1',
 ) inherits st2 {
-  if !defined(Class['postgresql::server']) {
+  if !defined(Class['::postgresql::server']) {
     if ($::osfamily == 'RedHat') and ($::operatingsystemmajrelease == '6') {
       class { '::postgresql::globals':
         version             => '9.4',
         manage_package_repo => true,
-      }
-
-      # Ugly hack, but sometimes this data directory is created (empty) before
-      # initdb is run, causing the postgres-server service to fail to start
-      # (only on CentOS 6).
-      # The same thing is present in the bootstrap-el6.sh script from
-      # st2 base.
-      exec { 'Move postgres data directory on first install':
-        command     => '/bin/mv /var/lib/pgsql/9.4/data /var/lib/pgsql/9.4/data.bck',
-        refreshonly => true,
-        subscribe   => Class['::postgresql::server::install'],
-        before      => Class['::postgresql::server::initdb'],
       }
     }
 
