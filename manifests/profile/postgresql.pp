e534a8b92cf72cb5ad0b559077a05bed57db5230
Nick Maludy <nmaludy@gmail.com>
2017-12-10 21:58:16 -0500

    Implemented a workaround for Postgres failing to start on initial creation in CentOS 6

--- manifests/profile/postgresql.pp
+++ manifests/profile/postgresql.pp
@@ -27,6 +27,18 @@ class st2::profile::postgresql(
         version             => '9.4',
         manage_package_repo => true,
       }
+
+      # Ugly hack, but sometimes this data directory is created (empty) before
+      # initdb is run, causing the postgres-server service to fail to start
+      # (only on CentOS 6).
+      # The same thing is present in the bootstrap-el6.sh script from
+      # st2 base.
+      exec { 'Move postgres data directory on first install':
+        command     => '/bin/mv /var/lib/pgsql/9.4/data /var/lib/pgsql/9.4/data.bck',
+        refreshonly => true,
+        subscribe   => Class['::postgresql::server::install'],
+        before      => Class['::postgresql::server::initdb'],
+      }
     }
 
     class { '::postgresql::server':
