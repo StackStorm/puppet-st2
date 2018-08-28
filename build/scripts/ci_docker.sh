05da7cdb428ee4ad58ac101657e698cb0ec7ab99
Nick Maludy <nmaludy@gmail.com>
2018-08-27 20:32:04 -0400

    Bugfix in build script

--- build/scripts/ci_docker.sh
+++ build/scripts/ci_docker.sh
@@ -3,9 +3,11 @@ set -e
 set -o xtrace
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $SCRIPT_DIR
 export TEST_NAME="$TEST_NAME"
+echo $TEST_NAME
 
-if [[ ! -z "$UNIT_TEST" ]]; then
+if [ ! -z "$UNIT_TEST" ]; then
   "$SCRIPT_DIR"/ci_docker_unit.sh
 else
   "$SCRIPT_DIR"/ci_docker_integration.sh
