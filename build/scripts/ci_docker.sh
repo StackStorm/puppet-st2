6fd6ac47075c91392d9529e1844bb22be116dc04
Nick Maludy <nmaludy@gmail.com>
2018-08-27 20:23:44 -0400

    Removed Puppet 3 tests. Added CentOS 6 test for Puppet 4 and 5

---
+++ build/scripts/ci_docker.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+set -o xtrace
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export TEST_NAME="$TEST_NAME"
+
+if [[ ! -z "$UNIT_TEST" ]]; then
+  "$SCRIPT_DIR"/ci_docker_unit.sh
+else
+  "$SCRIPT_DIR"/ci_docker_integration.sh
+fi
