03448876c76ec218ab7c76e4a14a813a7bfb560a
Nick Maludy <nmaludy@gmail.com>
2018-09-04 20:48:01 -0400

    Working on PDK unit testing

--- build/scripts/ci.sh
+++ build/scripts/ci.sh
@@ -3,14 +3,16 @@ set -e
 set -o xtrace
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-echo "SCRIPT_DIR = $SCRIPT_DIR"
+echo $SCRIPT_DIR
 
 if [ ! -z "$UNIT_TEST" ]; then
   export CHECK="$CHECK"
-  echo "CHECK = $CHECK"
+  echo $CHECK
+  export PUPPET_GEM_VERSION="$PUPPET_GEM_VERSION"
+  echo $PUPPET_GEM_VERSION
   "$SCRIPT_DIR"/ci_pdk_unit.sh
 else
   export TEST_NAME="$TEST_NAME"
-  echo "TEST_NAME = $TEST_NAME"
+  echo $TEST_NAME
   "$SCRIPT_DIR"/ci_docker_integration.sh
 fi
--- build/scripts/ci_docker.sh
+++ build/scripts/ci_docker.sh
@@ -3,14 +3,15 @@ set -e
 set -o xtrace
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-echo "SCRIPT_DIR = $SCRIPT_DIR"
 
 export TEST_NAME="$TEST_NAME"
-echo "TEST_DIR = $TEST_NAME"
+echo $TEST_NAME
 
 if [ ! -z "$UNIT_TEST" ]; then
   export CHECK="$CHECK"
-  echo "CHECK = $CHECK"
+  echo $CHECK
+  export PUPPET_GEM_VERSION="$PUPPET_GEM_VERSION"
+  echo $PUPPET_GEM_VERSION
   "$SCRIPT_DIR"/ci_docker_unit.sh
 else
   "$SCRIPT_DIR"/ci_docker_integration.sh
