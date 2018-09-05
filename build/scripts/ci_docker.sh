a9197b492ffe6a79a2b6ed8a18dddf77a8d36b8b
Nick Maludy <nmaludy@gmail.com>
2018-09-04 20:40:29 -0400

    Cleaned up echos in ci scripts

--- build/scripts/ci_docker.sh
+++ build/scripts/ci_docker.sh
@@ -3,12 +3,14 @@ set -e
 set -o xtrace
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-echo $SCRIPT_DIR
+echo "SCRIPT_DIR = $SCRIPT_DIR"
 
 export TEST_NAME="$TEST_NAME"
-echo $TEST_NAME
+echo "TEST_DIR = $TEST_NAME"
 
 if [ ! -z "$UNIT_TEST" ]; then
+  export CHECK="$CHECK"
+  echo "CHECK = $CHECK"
   "$SCRIPT_DIR"/ci_docker_unit.sh
 else
   "$SCRIPT_DIR"/ci_docker_integration.sh
