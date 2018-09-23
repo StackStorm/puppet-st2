cecb9348e1c6fc932dc1a57375615b9113fa9a95
Nick Maludy <nmaludy@gmail.com>
2018-09-22 23:18:16 -0400

    Fix rubocop errors

--- build/scripts/ci_pdk_unit.sh
+++ build/scripts/ci_pdk_unit.sh
@@ -4,4 +4,5 @@ set -o xtrace
 
 export CHECK="${CHECK:-syntax lint metadata_lint check:symlinks check:git_ignore check:dot_underscore check:test_file rubocop parallel_spec}"
 
+bundle install --without system_tests
 bundle exec rake $CHECK
--- spec/functions/urlencode_spec.rb
+++ spec/functions/urlencode_spec.rb
@@ -1,26 +1,27 @@
 # coding: utf-8
+
 require 'spec_helper'
 
 describe 'st2::urlencode' do
   context 'when checking parameter validity' do
     it { is_expected.not_to eq(nil) }
-    
+
     it 'when passing no arguments' do
       is_expected.to run.with_params.and_raise_error(ArgumentError, %r{expects 1 argument, got none})
     end
-    
-    it 'when more than one argument'do
+
+    it 'when more than one argument' do
       is_expected.to run.with_params('one', 'two').and_raise_error(ArgumentError, %r{expects 1 argument, got 2})
     end
-    
+
     it 'when passing an array (non-string)' do
       is_expected.to run.with_params([]).and_raise_error(ArgumentError)
     end
-    
+
     it 'when passing a hash (non-string)' do
       is_expected.to run.with_params({}).and_raise_error(ArgumentError)
     end
-    
+
     it 'when passing an integer (non-string)' do
       is_expected.to run.with_params(1).and_raise_error(ArgumentError)
     end
@@ -28,7 +29,7 @@ describe 'st2::urlencode' do
 
   context 'when urlencoding' do
     sample_text    = 'abc@/:+xyz'
-    desired_output = "abc%40%2F%3A%2Bxyz"
+    desired_output = 'abc%40%2F%3A%2Bxyz'
 
     it 'outputs URL encoded text' do
       is_expected.to run.with_params(sample_text).and_return(desired_output)
