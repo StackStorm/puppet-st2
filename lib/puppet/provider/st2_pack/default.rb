9391b7ffa0bed079df60c394aa5c2a772ae220c0
Nick Maludy <nmaludy@gmail.com>
2019-02-02 15:25:08 -0500

    Fixing st2_pack resource not escaping shell args

--- lib/puppet/provider/st2_pack/default.rb
+++ lib/puppet/provider/st2_pack/default.rb
@@ -1,15 +1,10 @@
 require 'json'
+require 'shellwords'
 
 Puppet::Type.type(:st2_pack).provide(:default) do
   desc 'Provides support for managing st2 packs'
 
   commands st2: '/usr/bin/st2'
-
-  def list_installed_packs
-    token = st2_authenticate
-    output = exec_st2('pack', 'list', '-a', 'name', '-j', '-t', token)
-    parse_output_json(output)
-  end
 
   # Get admin token
   def st2_authenticate
@@ -33,14 +28,20 @@ Puppet::Type.type(:st2_pack).provide(:default) do
     list_installed_packs.include?(@resource[:name])
   end
 
+  def list_installed_packs
+    token = st2_authenticate
+    output = exec_st2('pack', 'list', '-a', 'name', '-j', '-t', token)
+    parse_output_json(output)
+  end
+
   # Return list of package names
   def parse_output_json(raw)
     result = []
-    my_hash = JSON.parse(raw)
-    my_hash.each do |pack|
-      result << pack['name']
+    if raw && !raw.empty?
+      pack_list = JSON.parse(raw)
+      result = pack_list.map { |pack| pack['name'] }
+      debug("Installed packs: #{result}")
     end
-    debug("Installed packs: #{result}")
     result
   end
 
@@ -49,7 +50,9 @@ Puppet::Type.type(:st2_pack).provide(:default) do
   # execute the st2 command and use the system locale (UTF8)
   # so that the st2 CLI doesn't complain and throw errors
   def exec_st2(*args)
-    Puppet::Util::Execution.execute([command(:st2)] + args,
+    # escape all arguments so they're safe to use in a shell command
+    escaped_args = args.map { |a| Shellwords.shellescape(a) }
+    Puppet::Util::Execution.execute([command(:st2)] + escaped_args,
                                     override_locale: false)
   end
 end
