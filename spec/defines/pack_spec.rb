25788176a7a994517fbc16b9e09361b46ccebc01
Nick Maludy <nmaludy@gmail.com>
2020-08-17 21:31:37 -0400

    Remove fact helper because no longer needed

--- spec/defines/pack_spec.rb
+++ spec/defines/pack_spec.rb
@@ -1,98 +1,74 @@
 require 'spec_helper'
-require 'helpers/fact_helper'
 
 describe 'st2::pack' do
   let(:title) { 'st2testpack' }
-  let(:facts) do
-    {
-      os: {
-        family: 'RedHat',
-        release: {
-          major: '7',
-        },
-      },
-      operatingsystem: 'RedHat',
-      osfamily: 'RedHat',
-      operatingsystemmajrelease: '7',
-    }
-  end
 
-  context 'when declared with config' do
-    let(:params) do
-      {
-        repo_url: 'https://git.company.com/repos/packs/pack.git',
-        config:  {
-          foo: 'bar',
-        },
-      }
-    end
+  on_supported_os.each do |os, os_facts|
+    let(:facts) { os_facts }
 
-    it do
-      is_expected.to contain_st2_pack('st2testpack')
-    end
+    context "on #{os}" do
+      context 'when declared with config' do
+        let(:params) do
+          {
+            repo_url: 'https://git.company.com/repos/packs/pack.git',
+            config: {
+              foo: 'bar',
+            },
+          }
+        end
 
-    it do
-      is_expected.to contain_file('/opt/stackstorm/configs/st2testpack.yaml').with(
-        ensure: 'file',
-        owner: 'st2',
-        group: 'root',
-        mode: '0640',
-      )
-    end
-  end
+        it { is_expected.to contain_st2_pack('st2testpack') }
+        it do
+          is_expected.to contain_file('/opt/stackstorm/configs/st2testpack.yaml')
+            .with(ensure: 'file',
+                  owner: 'st2',
+                  group: 'root',
+                  mode: '0640')
+        end
+      end
 
-  context 'when declared with default parameters' do
-    it do
-      is_expected.to contain_st2_pack('st2testpack').with(
-        ensure: 'present',
-        user: 'st2admin',
-        password: 'Ch@ngeMe',
-      )
-    end
-  end
+      context 'when declared with default parameters' do
+        it do
+          is_expected.to contain_st2_pack('st2testpack')
+            .with(ensure: 'present',
+                  user: 'st2admin',
+                  password: 'Ch@ngeMe')
+        end
+      end
 
-  context 'when declared with explicit source' do
-    let(:params) do
-      {
-        repo_url: 'https://git.company.com/repos/packs/pack.git',
-      }
-    end
+      context 'when declared with explicit source' do
+        let(:params) { { repo_url: 'https://git.company.com/repos/packs/pack.git' } }
 
-    it do
-      is_expected.to contain_st2_pack('st2testpack').with(
-        ensure: 'present',
-        user: 'st2admin',
-        password: 'Ch@ngeMe',
-        source: 'https://git.company.com/repos/packs/pack.git',
-      )
-    end
-  end
+        it do
+          is_expected.to contain_st2_pack('st2testpack')
+            .with(ensure: 'present',
+                  user: 'st2admin',
+                  password: 'Ch@ngeMe',
+                  source: 'https://git.company.com/repos/packs/pack.git')
+        end
+      end
 
-  context 'when declared with non-default user and password' do
-    let(:pre_condition) { 'class {"st2": cli_password => "test_bar", cli_username => "test_foo"}' }
+      context 'when declared with non-default user and password' do
+        let(:pre_condition) { 'class {"st2": cli_password => "test_bar", cli_username => "test_foo"}' }
 
-    it do
-      is_expected.to contain_st2_pack('st2testpack').with(
-        ensure: 'present',
-        user: 'test_foo',
-        password: 'test_bar',
-      )
-    end
-  end
+        it do
+          is_expected.to contain_st2_pack('st2testpack')
+            .with(ensure: 'present',
+                  user: 'test_foo',
+                  password: 'test_bar')
+        end
+      end
 
-  context 'when defining a package absent' do
-    let(:params) do
-      {
-        ensure: 'absent',
-      }
-    end
+      context 'when defining a package absent' do
+        let(:params) { { ensure: 'absent' } }
 
-    it do
-      is_expected.to contain_st2_pack('st2testpack').with(
-        ensure: 'absent',
-        user: 'st2admin',
-        password: 'Ch@ngeMe',
-      )
+        it do
+          is_expected.to contain_st2_pack('st2testpack')
+            .with(ensure: 'absent',
+                  user: 'st2admin',
+                  password: 'Ch@ngeMe')
+        end
+      end
     end
   end
 end
--- spec/functions/version_ge_spec.rb
+++ spec/functions/version_ge_spec.rb
@@ -3,172 +3,94 @@
 require 'spec_helper'
 
 describe 'st2::version_ge' do
-  context 'when StackStorm is not installed' do
-    let(:facts) do
-      {
-        st2_version: nil,
-        os: {
-          family: 'RedHat',
-          release: {
-            major: '7',
-          },
-        },
-      }
-    end
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      context 'when StackStorm is not installed' do
+        let(:facts) { os_facts.merge(st2_version: nil) }
 
-    it 'comparing with any version returns true' do
-      is_expected.to run.with_params('2.10.0').and_return(true)
-    end
-  end
-
-  context 'when StackStorm is installed' do
-    context 'and st2::version = "present"' do
-      let(:pre_condition) { "class {'st2': version => 'present' }" }
-      let(:facts) do
-        {
-          st2_version: '2.9.0',
-          'st2::version' => 'present',
-          osfamily: 'RedHat',
-          os: {
-            family: 'RedHat',
-            release: {
-              major: '7',
-            },
-          },
-        }
+        it 'comparing with any version returns true' do
+          is_expected.to run.with_params('2.10.0').and_return(true)
+        end
       end
 
-      it 'comparing with higher version returns false' do
-        is_expected.to run.with_params('2.10.0').and_return(false)
-      end
+      context 'when StackStorm is installed' do
+        context 'and st2::version = "present"' do
+          let(:pre_condition) { "class {'st2': version => 'present' }" }
+          let(:facts) { os_facts.merge(st2_version: '2.9.0', 'st2::version' => 'present') }
 
-      it 'comparing with lower version returns true' do
-        is_expected.to run.with_params('2.8.0').and_return(true)
-      end
-    end
+          it 'comparing with higher version returns false' do
+            is_expected.to run.with_params('2.10.0').and_return(false)
+          end
 
-    context 'and st2::version = "installed"' do
-      let(:pre_condition) { "class {'st2': version => 'installed' }" }
-      let(:facts) do
-        {
-          st2_version: '2.9.0',
-          'st2::version' => 'installed',
-          osfamily: 'RedHat',
-          os: {
-            family: 'RedHat',
-            release: {
-              major: '7',
-            },
-          },
-        }
-      end
+          it 'comparing with lower version returns true' do
+            is_expected.to run.with_params('2.8.0').and_return(true)
+          end
+        end
 
-      it 'comparing with higher version (2.10.0) returns false' do
-        is_expected.to run.with_params('2.10.0').and_return(false)
-      end
+        context 'and st2::version = "installed"' do
+          let(:pre_condition) { "class {'st2': version => 'installed' }" }
+          let(:facts) { os_facts.merge(st2_version: '2.9.0', 'st2::version' => 'installed') }
 
-      it 'comparing with lower version (2.8.0) returns true' do
-        is_expected.to run.with_params('2.8.0').and_return(true)
-      end
-    end
+          it 'comparing with higher version (2.10.0) returns false' do
+            is_expected.to run.with_params('2.10.0').and_return(false)
+          end
 
-    context 'and st2::version = "latest"' do
-      let(:pre_condition) { "class {'st2': version => 'latest' }" }
-      let(:facts) do
-        {
-          st2_version: '2.9.0',
-          'st2::version' => 'latest',
-          osfamily: 'RedHat',
-          os: {
-            family: 'RedHat',
-            release: {
-              major: '7',
-            },
-          },
-        }
-      end
+          it 'comparing with lower version (2.8.0) returns true' do
+            is_expected.to run.with_params('2.8.0').and_return(true)
+          end
+        end
 
-      it 'comparing with higher version (2.10.0) returns true' do
-        is_expected.to run.with_params('2.10.0').and_return(true)
-      end
+        context 'and st2::version = "latest"' do
+          let(:pre_condition) { "class {'st2': version => 'latest' }" }
+          let(:facts) { os_facts.merge(st2_version: '2.9.0', 'st2::version' => 'latest') }
 
-      it 'comparing with lower version (2.8.0) returns true' do
-        is_expected.to run.with_params('2.8.0').and_return(true)
-      end
-    end
+          it 'comparing with higher version (2.10.0) returns true' do
+            is_expected.to run.with_params('2.10.0').and_return(true)
+          end
 
-    context 'and st2_version = "2.9.0" st2::version = "2.9.0"' do
-      let(:pre_condition) { "class {'st2': version => '2.9.0' }" }
-      let(:facts) do
-        {
-          st2_version: '2.9.0',
-          'st2::version' => '2.9.0',
-          osfamily: 'RedHat',
-          os: {
-            family: 'RedHat',
-            release: {
-              major: '7',
-            },
-          },
-        }
-      end
+          it 'comparing with lower version (2.8.0) returns true' do
+            is_expected.to run.with_params('2.8.0').and_return(true)
+          end
+        end
 
-      it 'comparing with higher version (2.10.0) returns false' do
-        is_expected.to run.with_params('2.10.0').and_return(false)
-      end
+        context 'and st2_version = "2.9.0" st2::version = "2.9.0"' do
+          let(:pre_condition) { "class {'st2': version => '2.9.0' }" }
+          let(:facts) { os_facts.merge(st2_version: '2.9.0', 'st2::version' => '2.9.0') }
 
-      it 'comparing with lower version (2.8.0) returns true' do
-        is_expected.to run.with_params('2.8.0').and_return(true)
-      end
-    end
+          it 'comparing with higher version (2.10.0) returns false' do
+            is_expected.to run.with_params('2.10.0').and_return(false)
+          end
 
-    context 'and st2_version = "2.9.0" st2::version = "2.10.0"' do
-      let(:pre_condition) { "class {'st2': version => '2.10.0' }" }
-      let(:facts) do
-        {
-          st2_version: '2.9.0',
-          'st2::version' => '2.10.0',
-          osfamily: 'RedHat',
-          os: {
-            family: 'RedHat',
-            release: {
-              major: '7',
-            },
-          },
-        }
-      end
+          it 'comparing with lower version (2.8.0) returns true' do
+            is_expected.to run.with_params('2.8.0').and_return(true)
+          end
+        end
 
-      it 'comparing with higher version (2.11.0) returns false' do
-        is_expected.to run.with_params('2.11.0').and_return(false)
-      end
+        context 'and st2_version = "2.9.0" st2::version = "2.10.0"' do
+          let(:pre_condition) { "class {'st2': version => '2.10.0' }" }
+          let(:facts) { os_facts.merge(st2_version: '2.9.0', 'st2::version' => '2.10.0') }
 
-      it 'comparing with lower version (2.8.0) returns true' do
-        is_expected.to run.with_params('2.8.0').and_return(true)
-      end
-    end
+          it 'comparing with higher version (2.11.0) returns false' do
+            is_expected.to run.with_params('2.11.0').and_return(false)
+          end
 
-    context 'and st2_version = "2.10.0" st2::version = "2.9.0"' do
-      let(:pre_condition) { "class {'st2': version => '2.9.0' }" }
-      let(:facts) do
-        {
-          st2_version: '2.10.0',
-          'st2::version' => '2.9.0',
-          osfamily: 'RedHat',
-          os: {
-            family: 'RedHat',
-            release: {
-              major: '7',
-            },
-          },
-        }
-      end
+          it 'comparing with lower version (2.8.0) returns true' do
+            is_expected.to run.with_params('2.8.0').and_return(true)
+          end
+        end
 
-      it 'comparing with higher version (2.11.0) returns false' do
-        is_expected.to run.with_params('2.11.0').and_return(false)
-      end
+        context 'and st2_version = "2.10.0" st2::version = "2.9.0"' do
+          let(:pre_condition) { "class {'st2': version => '2.9.0' }" }
+          let(:facts) { os_facts.merge(st2_version: '2.10.0', 'st2::version' => '2.9.0') }
 
-      it 'comparing with lower version (2.8.0) returns true' do
-        is_expected.to run.with_params('2.8.0').and_return(true)
+          it 'comparing with higher version (2.11.0) returns false' do
+            is_expected.to run.with_params('2.11.0').and_return(false)
+          end
+
+          it 'comparing with lower version (2.8.0) returns true' do
+            is_expected.to run.with_params('2.8.0').and_return(true)
+          end
+        end
       end
     end
   end
