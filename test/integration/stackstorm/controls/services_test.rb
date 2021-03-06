68c93f3041f088993c4c4a7546ebae8491983678
Bradley Bishop <bradley.bishop@encore.tech>
2021-03-05 22:22:50 -0500

    st2resultstracker was removed from ST2 3.4

--- test/integration/stackstorm/controls/services_test.rb
+++ test/integration/stackstorm/controls/services_test.rb
@@ -7,7 +7,7 @@
 # https://github.com/StackStorm/st2/blob/master/st2common/bin/st2ctl#L5
 ST2_SERVICES = ['st2actionrunner', 'st2api', 'st2stream',
                 'st2auth', 'st2garbagecollector', 'st2notifier',
-                'st2resultstracker', 'st2rulesengine', 'st2sensorcontainer',
+                'st2rulesengine', 'st2sensorcontainer',
                 'st2workflowengine', 'st2timersengine', 'st2scheduler'].freeze
 
 control 'st2-services' do
