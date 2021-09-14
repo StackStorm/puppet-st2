c6c3cdac4c2afd3de7702d8be14a6c9fa6da7db9
Ankur Singh <ankur.singh@cern.ch>
2021-09-14 13:46:08 +0200

    fix rubocop offense

--- spec/classes/profile/mongodb_spec.rb
+++ spec/classes/profile/mongodb_spec.rb
@@ -44,7 +44,7 @@ describe 'st2::profile::mongodb' do
 
       context 'with st2_version == 3.3.0' do
         let(:facts) { os_facts.merge('st2_version' => '3.3.0') }
-        
+
         it do
           if os == 'ubuntu-20.04-x86_64'
             is_expected.to contain_class('mongodb::globals')
